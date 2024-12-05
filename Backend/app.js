177e51782d73084818845525c17f7dc00bcac399
KalashT <kalashthakare898@gmail.com>
2024-12-05 23:29:30 +0530

    db connection

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cors from 'cors';
+import connectToDb from './db/db.js';
 
+connectToDb();
 const app=express();
 
 app.use(cors());
