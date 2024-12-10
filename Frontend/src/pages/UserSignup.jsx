fae127489e111a6a26ef30ea220701db8d66151e
KalashT <kalashthakare898@gmail.com>
2024-12-11 00:26:06 +0530

    user-signup

--- Frontend/src/pages/UserSignup.jsx
+++ Frontend/src/pages/UserSignup.jsx
@@ -1,8 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const UserSignup = () => {
+
+  const [email, setemail] = useState('')
+  const [Firstname, setFirstname] = useState('')
+  const [lastname, setlastname] = useState('')
+  const [password, setpassword] = useState('')
+
+  const submitHandler=(e)=>{
+    e.preventDefault();
+  }
+
+
   return (
-    <div>UserSignup</div>
+    <div className='p-7 flex flex-col justify-between h-screen'>
+      <div>
+        <img className='w-[4rem] mb-7' src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Uber_logo_2018.png/1200px-Uber_logo_2018.png" alt="" />
+
+        <form action="" onSubmit={submitHandler}>
+
+        <h3 className='text-xl mb-2 font-semibold'>Enter your fullname</h3>
+
+          <div className='flex gap-4'>
+
+            <input 
+              value={Firstname}
+              onChange={(e)=>{
+                setFirstname(e.target.value);
+              }}
+              className='bg-[#eeeeee] rounded px-4 py-2 border w-1/2 text-lg placeholder:text-base mb-5'
+              required type="text"
+              placeholder='Firstname'
+            />
+
+            <input 
+              value={lastname}
+              onChange={(e)=>{
+                setlastname(e.target.value);
+              }}
+              className='bg-[#eeeeee] rounded px-4 py-2 border w-1/2 text-lg placeholder:text-base mb-5'
+              required type="text"
+              placeholder='Lastname'
+            />
+
+          </div>
+
+          <h3 className='text-xl mb-2 font-semibold'>What's your email</h3>
+
+          <input 
+            value={email}
+            onChange={(e)=>{
+              setemail(e.target.value);
+            }}
+            className='bg-[#eeeeee] rounded px-4 py-2 border w-full text-lg placeholder:text-base mb-5'
+            required type="email"
+            placeholder='email@example.com'
+          />
+
+
+          <h3 className='text-xl mb-2 font-semibold'>Enter Password</h3>
+          <input 
+            value={password}
+            onChange={(e)=>{
+              setpassword(e.target.value);
+            }}
+            className='bg-[#eeeeee] rounded px-4 py-2 border w-full text-lg placeholder:text-base mb-5'
+            required type="password" 
+            placeholder='password' 
+          />
+
+          <button className='text-white bg-[#111] font-semibold mb-7 rounded px-4 py-2 w-full'>
+            Sign in
+          </button>
+          
+          <p>
+            Alredy have a account?
+            <Link 
+              to='/user-login'
+              className='text-blue-600'
+            >
+              Login here
+            </Link>
+          </p>
+
+        </form>
+      </div>
+
+      {/* <div>
+        <Link
+          to='/user-login'
+          className='flex justify-center items-center text-white bg-[#d5622d] font-semibold mb-7 rounded px-4 py-2 w-full'>
+          Log in as User
+        </Link>
+      </div> */}
+
+    </div>
   )
 }
 
